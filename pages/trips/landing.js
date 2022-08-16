5ed00b5cffe72b339da2b0fa48b69c109156c17d
Julian Harris <vanillites@protonmail.com>
2022-08-16 14:35:55 +0800

    index page still in progress

--- pages/trips/landing.js
+++ pages/trips/landing.js
@@ -5,7 +5,24 @@ Page({
    * Page initial data
    */
   data: {
-
+    trips: [
+      {
+        title: "Yoga retreat",
+        description: "Come to the shelter and meet your new best friend. Snacks and drinks provided!",
+        date: "August 2nd",
+        time: "15:00",
+        location: "Bali",
+        image: "https://images.unsplash.com/photo-1555400038-63f5ba517a47?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+      },
+      {
+        title: "End-of-Summer Party",
+        description: "Come along with your fur baby! Good food and great company all in support of HiPaw. If you don't have a pet, there will be some there for you to meet too!",
+        date: "August 28th",
+        time: "14:00",
+        location: "Bali",
+        image: "https://images.unsplash.com/photo-1577717903315-1691ae25ab3f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+      }
+    ]
   },
 
   /**
