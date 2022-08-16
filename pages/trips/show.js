938d9d04f42ce6e18855cb67258b85f296df56b3
Ann Altantulga <a3jargal@icloud.com>
2022-08-16 16:34:14 +0800

    wxml completed

---
+++ pages/trips/show.js
@@ -0,0 +1,139 @@
+// pages/trips/show.js
+const app = getApp()
+Page({
+  data: {
+    date: "",
+    time: "",
+    showBookingModal: false,
+    booking: {},
+    // isAdopter: false,
+    // isCreater: false,
+  },
+  onLoad(options) {
+
+  },
+  onReady() {
+
+  },
+  onShow() {
+    let page = this
+    console.log('From show.js - onshow: options ', this.options)
+    let id = this.options.id
+    wx.request({
+      header: app.globalData.header,
+      url: `${app.globalData.baseURL}/trips/${id}`,
+      success(res) {
+        console.log("From show.js - onshow: res",res)
+        if (res.statusCode === 200) {
+          console.log("From show.js - onshow: booking", res.data.my_booking);
+          console.log("From show.js - onshow: trip's user_id", res.data.trip.user_id)
+         
+          const trip = res.data.trip;
+          const booking = res.data.my_booking;
+          
+          // const date = new Date()
+          // const date = Date(booking.date_and_time)
+          page.setData({
+            trip: trip,
+            isCreater: app.globalData.user.id === trip.user_id,
+            // isAdopter: app.globalData.user.id !== pet.user_id,
+            isBooker: booking, 
+            booking: booking,
+          });
+          console.log("From show.js: status code is", res.statusCode)
+        }
+      }
+    })
+  },
+  submitBooking(e){
+    console.log("From show.js - submitBooking: e", e)
+    // const dateAndTime = new Date(`${this.data.date} ${this.data.time}`)
+    // console.log(dateAndTime)
+    let page = this
+    wx.request({
+      url: `${app.globalData.baseURL}/trips/${this.data.trip.id}/bookings`,
+      header: app.globalData.header,
+      method: "POST",
+      data: {
+        // date_and_time: dateAndTime
+      },
+      success(res){
+        console.log("From show.js - submit booking: res",res)
+        if (res.statusCode === 201) {
+          console.log("From show.js - booking submit successfully!")
+          console.log("From show.js : res.data", res.data)
+          const booking = res.data.booking;
+          console.log(page)
+          wx.redirectTo({
+            url: `/pages/users/profile?id=${page.options.id}`,
+          })
+          // console.log("test date: ", page.data.date)
+        } else {
+          console.log("From show.js: status code is", res.statusCode)
+          console.log("From show.js: error message", res.data.errors)
+          // const bookingId = res.data.booking.id
+          wx.showModal({
+            title: 'Error!',
+            content: res.data.errors.join(', '),
+            cancelText: "OK",
+            confirmText: 'Details',
+            success(res) {
+              console.log(res)
+              if (res.confirm) {
+                wx.redirectTo({
+                  url: `../booking/show?id=${bookingId}`,
+                })
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  onHide() {
+
+  },
+  onUnload() {
+
+  },
+  onPullDownRefresh() {
+
+  },
+  onReachBottom() {
+
+  },
+  onShareAppMessage() {
+
+  }, 
+  edit(e) {
+    wx.setStorageSync('editedId', this.data.trip.id)
+    console.log(this.data.trip)
+    wx.switchTab({
+      header: app.globalData.header,
+      url: 'pages/trips/form',
+    })
+  },
+
+  delete(e) {
+    let id = this.data.pet.id
+    wx.showModal({
+      title: 'Are you sure?',
+      content: 'Are you sure to delete this trip?',
+      success(res) {
+        if (res.confirm) {
+          wx.request({
+            header: app.globalData.header,
+            url: `${app.globalData.baseURL}/trips/${id}`,
+            method: 'DELETE',
+            success(res){
+              wx.switchTab({
+                url: '/pages/trips/landing',
+              })
+            }
+          })
+        } else {
+        }
+      }
+    })
+  },
+})
