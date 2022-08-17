2369c658adc1f6f657f8acac78d4862ae81a45f3
zora dong <zoradongccc@gmail.com>
2022-08-17 17:12:06 +0800

    add connect js with backend in the show page + add create & remove bookmark in the show page

--- pages/trips/show.js
+++ pages/trips/show.js
@@ -11,24 +11,35 @@ Page({
 
   },
   onShow() {
+    if (app.globalData.header) {
+      // proceed to fetch api
+      this.getData()
+    } else {
+      // wait until loginFinished, then fetch API
+      wx.event.on('loginFinished', this, this.getData)
+    }
+  }, 
+  getData() {
     let page = this
-    console.log('From show.js - onshow: options ', this.options)
-    let id = this.options.id
+    console.log('From show.js - onshow: options ', page.options)
+    let id = page.options.id
     wx.request({
       header: app.globalData.header,
       url: `${app.globalData.baseURL}/trips/${id}`,
       success(res) {
         console.log("From show.js - onshow: res",res)
         if (res.statusCode === 200) {
-          console.log("From show.js - onshow: booking", res.data.my_booking);
-          console.log("From show.js - onshow: trip's user_id", res.data.trip.user_id)
+          // console.log("From show.js - onshow: booking", res.data.my_booking);
+          // console.log("From show.js - onshow: trip's user_id", res.data.trip.user_id)
           const trip = res.data.trip;
-          const booking = res.data.my_booking;
+          const isBooker = res.data.is_booker;
+          const isSaved = res.data.is_saved;
+          // const booking = res.data.my_booking;
           page.setData({
             trip: trip,
-            isCreater: app.globalData.user.id === trip.user_id,
-            isBooker: booking, 
-            booking: booking,
+            isBooker: isBooker, 
+            isSaved: isSaved,
+            bookmarkId: res.data.bookmark_id
           });
           console.log("From show.js: status code is", res.statusCode)
         }
@@ -92,15 +103,64 @@ Page({
   onShareAppMessage() {
 
   }, 
-  // bookmark(e) {
-  //   console.log("book ", e)
-  //   let page = this 
-  //   wx.request({
-  //     url: 'url',
-  //   })
-  //   wx.switchTab({
-  //     header: app.globalData.header,
-  //     url: 'users/mytrips',
-  //   })
-  // }
+  toggleBookmark(e) {
+    console.log("From toggleBookmark e", e)
+    let page = this 
+    if (page.data.isSaved){
+      wx.request({
+        url: `${app.globalData.baseURL}/bookmarks/${page.data.bookmarkId}`,
+        method: 'DELETE',
+        header: app.globalData.header,
+        success(res) {
+          console.log("From toggleBookmark res",res)
+          if (res.statusCode === 200) {
+            console.log("bookmark removed")
+            page.setData({
+              isSaved: false,
+              bookmarkId: null
+            })
+            wx.showToast({
+              title: "remove successfully",
+              duration: 1000
+            })  
+          } else {
+            console.log("bookmark remove failed")
+            // wx.showToast({
+            //   title: "remove fails",
+            //   icon: "warn",
+            //   duration: 2000
+            // })  
+            // wx.reLaunch
+            // wx.navigateTo({
+            //   url: '',
+            // })
+          }
+
+        }
+      })
+    } else {
+      wx.request({
+        url: `${app.globalData.baseURL}/trips/${page.data.trip.id}/bookmarks`,
+        method: 'POST',
+        header: app.globalData.header,
+        success(res) {
+          console.log("From toggleBookmark res",res)
+          if (res.statusCode === 201) {
+            console.log("bookmark added")
+            let bookmark = res.data
+            page.setData({
+              isSaved: true,
+              bookmarkId: bookmark.id
+            })
+            wx.showToast({
+              title: "save successfully",
+              duration: 1000
+            })            
+          } else {
+            console.log("bookmark saved failed")
+          }
+        }
+      })
+    }
+  }
 })
