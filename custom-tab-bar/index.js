5b4618c2d6a76b60bdfcfb97a2bffefe910ef4e7
Julian Harris <vanillites@protonmail.com>
2022-08-17 10:12:50 +0800

    index page WIP - day 1 progress

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -1,23 +1,37 @@
 // custom-tab-bar/index.js
+let app = getApp();
 Component({
-  /**
-   * Component properties
-   */
-  properties: {
-
-  },
-
-  /**
-   * Component initial data
-   */
   data: {
-
+    selected: 0,
+    color: "#FFD6E0",
+    selectedColor: "#E15B99",
+    list: [{
+      pagePath: "/pages/trips/landing",
+      iconPath: "/images/homeempty-lightpink.png",
+      selectedIconPath: "/images/homefill-pink.png",
+      text: "Home"
+    }, {
+      pagePath: "/pages/users/mytrips",
+      iconPath: "/images/mytrips-lightpink.png",
+      selectedIconPath: "/images/mytrips-pink.png",
+      text: "My Trips"
+      }, {
+      pagePath: "/pages/users/profile",
+      iconPath: "/images/meempty-lightpink.png",
+      selectedIconPath: "/images/mefill-pink.png",
+      text: "Me"
+      }]
   },
-
-  /**
-   * Component methods
-   */
+  attached() {
+  },
   methods: {
-
+    switchTab(e) {
+      const data = e.currentTarget.dataset
+      const url = data.path
+      wx.switchTab({url})
+      this.setData({
+        selected: data.index
+      })
+    }
   }
 })
