1edd153dbad2fa98197a1992ac35755041ca9e22
Julian Harris <vanillites@protonmail.com>
2022-08-17 17:13:44 +0800

    add tag list to global data

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ App({
     userInfo: null, 
     header: null, 
     user: null,
-    baseURL: "http://localhost:3000/api/v1"
-    // baseURL: "http://triptrip.wogengapp.cn/api/v1"
+    baseURL: "http://localhost:3000/api/v1",
+    // baseURL: "http://triptrip.wogengapp.cn/api/v1",
+    tagList: ["oneday", "weekend", "petfriendly", "hiking", "relaxing", "family", "adventure", "biking"]
   }
 })
